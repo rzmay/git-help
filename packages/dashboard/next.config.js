202d68887cd960cd63a5325f8a8a1e024efdc0e8
Nathan A <nathanbammerman@gmail.com>
2024-02-02 20:10:44 -0500

    module not found fixed

--- packages/dashboard/next.config.js
+++ packages/dashboard/next.config.js
@@ -1,9 +1,9 @@
 const bundleAnalyzer = require('@next/bundle-analyzer');
 const transpileModules = require('next-transpile-modules');
-const withPlugins = require('lib/helpers/withPlugins');
+const withPlugins = require('./src/helpers/withPlugins');
 
 module.exports = withPlugins([
-  transpileModules(['lib']),
+  transpileModules(['shared']),
   bundleAnalyzer({ enabled: process.env.ANALYZE === 'true' }),
 ], {
   // swcMinify: true,
@@ -15,35 +15,35 @@ module.exports = withPlugins([
   images: {
     domains: ['*', 'www.gravatar.com', 'lh3.googleusercontent.com', 'i.seadn.io'],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.API_BASE_URL}/:path*`,
-      },
-      {
-        source: '/ajax/auth/:path*',
-        destination: `${process.env.API_BASE_URL}/auth/:path*`,
-      },
-      {
-        source: '/ajax/:path*',
-        destination: `${process.env.API_BASE_URL}/dashboard/:path*`,
-      },
-      {
-        source: '/invites/:invite_code',
-        destination: `${process.env.API_BASE_URL}/dashboard/invites/:invite_code`,
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/(.*\\/\\/.*)',
-        destination: '/',
-        permanent: false,
-      },
-    ];
-  },
+// async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: `${process.env.API_BASE_URL}/:path*`,
+//       },
+//       {
+//         source: '/ajax/auth/:path*',
+//         destination: `${process.env.API_BASE_URL}/auth/:path*`,
+//       },
+//       {
+//         source: '/ajax/:path*',
+//         destination: `${process.env.API_BASE_URL}/dashboard/:path*`,
+//       },
+//       {
+//         source: '/invites/:invite_code',
+//         destination: `${process.env.API_BASE_URL}/dashboard/invites/:invite_code`,
+//       },
+//     ];
+//   },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/(.*\\/\\/.*)',
+  //       destination: '/',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
   async headers() {
     return [{
       source: '/(.*)',
---
+++ packages/dashboard/src/pages/index.js
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Our Site</h1>
+            <p>This is the first page of our Next.js app.</p>
+        </div>
+    );
+}
+
+export default HomePage;    
