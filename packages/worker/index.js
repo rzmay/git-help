39f1277b4ee70049e2a84932279c65661eea637c
rzmay <robertmay2003@gmail.com>
2024-02-03 11:43:26 -0500

    Comment out pubsub

--- packages/worker/index.js
+++ packages/worker/index.js
@@ -1,10 +1,10 @@
 const Queue = require('bull');
 const mongoose = require('lib/mongoose');
-const pubsub = require('lib/pubsub');
+// const pubsub = require('lib/pubsub');
 const redis = require('lib/redis');
 const throng = require('throng');
 const issueHandler = require('./src/queues/issue');
-const issueBacklogHandler = require('./src/subscriptions/issueBacklog');
+// const issueBacklogHandler = require('./src/subscriptions/issueBacklog');
 
 async function start() {
   // Connect to database
@@ -17,13 +17,13 @@ async function start() {
   const issueQueue = new Queue('issue', process.env.REDIS_URL);
 
   // Connect to GCS subscriptions
-  const issueBacklogSub = pubsub('issue-backlog');
+  // const issueBacklogSub = pubsub('issue-backlog');
 
   // Attach work handlers to queues
   issueQueue.process(50, issueHandler);
 
   // Attach work handlers to subscriptions
-  issueBacklogSub.on('message', issueBacklogHandler);
+  // issueBacklogSub.on('message', issueBacklogHandler);
 }
 
 throng({
